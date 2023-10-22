aebbbde4f7f195f88e08ab8745dfc6192437c25b
Adi Lev <lev733@gmail.com>
2023-10-22 11:35:47 +0300

    Fixed auth context provider issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import { AppProvider } from "@/store/AppProvider";
+import { FavMoviesProvider } from "@/store/fav-movies-context";
 import ErrorBoundary from "./ErrorBoundary";
+import { setApiAuth } from "@/services/auth-service";
+import { checkApiAuth } from "@/utils";
+import { CircularProgress } from "@mui/material";
+
+
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  const setAuthHandler = async () => {
+    await setApiAuth();
+    setIsLoggedIn(checkApiAuth);
+  };
+  
+  useEffect(() => {
+    setAuthHandler();
+  }, []);
+
   return (
     <div className="App">
       <ErrorBoundary>
-        <AppProvider>
-          <Outlet />
-        </AppProvider>
+        {isLoggedIn ? (
+          <FavMoviesProvider>
+            <Outlet />
+          </FavMoviesProvider>
+        ) : (
+          <CircularProgress />
+        )}
       </ErrorBoundary>
     </div>
   );
